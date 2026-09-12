28b3ef772f7541684f7f414b51c9277acc5a17d9
SebasG789 <sebas35perez@gmail.com>
2026-09-11 19:27:40 -0500

    Se crea la carpeta web para desplegar en vercel

--- Scripts/Player/player.gd
+++ Scripts/Player/player.gd
@@ -1,44 +1,36 @@
 class_name Player
 extends CharacterBody2D
 
-@export var SPEED: float = 60.0
+@export var Speed: float = 60.0
 @export var is_attacking : bool = false
-@export var facing_direction:= Vector2.DOWN
+@export var facing_direction := Vector2.DOWN
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
 func _physics_process(delta: float) -> void:
 	
 	if Input.is_action_just_pressed("ATACAR"):
 		attack()
-	
+		
 	if is_attacking:
 		return
 	
-	var direction = Input.get_vector(
-	 "IZQUIERDA",
-	 "DERECHA",
-	 "ARRIBA",
-	 "ABAJO")
-	velocity = direction * SPEED
-	
-	if direction.x >0:
+	var direction = Input.get_vector("IZQUIERDA","DERECHA","ARRIBA","ABAJO")
+	velocity = direction * Speed
+	if direction.x > 0:
 		facing_direction = Vector2.RIGHT
 		animated_sprite_2d.play("walk_right")
-	elif direction.x < 0:
+	elif  direction.x < 0:
 		facing_direction = Vector2.LEFT
 		animated_sprite_2d.play("walk_left")
-	elif direction.y >0:
+	elif direction.y > 0:
 		facing_direction = Vector2.DOWN
 		animated_sprite_2d.play("walk_down")
-	elif direction.y <0:
+	elif direction.y < 0 :
 		facing_direction = Vector2.UP
 		animated_sprite_2d.play("walk_up")
-	
 	move_and_slide()
+	#Cunado el jugador este quieto, animacion donde este mirando luego se pausa la animacion
 	
-	#Cuando el player este quieto, ejecutamos la animacion 
-	#hacia la direccion donde este mirando 
-	#y luego pausamos la animacion
 	if direction == Vector2.ZERO:
 		if facing_direction == Vector2.UP:
 			animated_sprite_2d.play("walk_up")
@@ -50,19 +42,16 @@ func _physics_process(delta: float) -> void:
 			animated_sprite_2d.play("walk_right")
 		animated_sprite_2d.pause()
 		return
-		
+	
 func attack() -> void:
 	is_attacking = true
-	
 	if facing_direction == Vector2.UP:
 		animated_sprite_2d.play("attack_up")
 	elif facing_direction == Vector2.DOWN:
 		animated_sprite_2d.play("attack_down")
 	elif facing_direction == Vector2.LEFT:
-		animated_sprite_2d.play("attack_left")	
+		animated_sprite_2d.play("attack_left")
 	elif facing_direction == Vector2.RIGHT:
 		animated_sprite_2d.play("attack_right")
-		
-		await animated_sprite_2d.animation_finished
-		
-		is_attacking = false
+	await  animated_sprite_2d.animation_finished
+	is_attacking = false
