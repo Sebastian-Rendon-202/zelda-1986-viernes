e6f216ccf836b3bf98268ac50d9faf4171d78a79
SebasG789 <sebas35perez@gmail.com>
2026-09-11 21:25:15 -0500

    Actualizacion

---
+++ Scenes/World/screen_transition.gd
@@ -0,0 +1,21 @@
+class_name Screen_Transition 
+extends Node2D
+
+@onready var camera_2d: Camera2D = $".."
+@onready var player: Player = $"../../Player"
+
+
+func _on_area_2d_up_body_entered(body: Node2D) -> void:
+	pass # Replace with function body.
+
+
+func _on_area_2d_down_body_entered(body: Node2D) -> void:
+	pass # Replace with function body.
+
+
+func _on_area_2d_left_body_entered(body: Node2D) -> void:
+	pass # Replace with function body.
+
+
+func _on_area_2d_4_right_body_entered(body: Node2D) -> void:
+	pass # Replace with function body.
